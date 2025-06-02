d6deea559e481f98d681fe9986f71090e9888e29
sannasi marimuthu <sannasimarimuthu21@gmail.com>
2025-06-02 21:16:59 +0530

    display name normal

--- Schema/Property/PropertySchema.js
+++ Schema/Property/PropertySchema.js
@@ -5,7 +5,8 @@ const Schema = new mongoose.Schema(
     Propertyname: { type: String, required: true }, //
     Propertydescription: { type: String, required: false },
     Yearofconstruction: { type: Number, required: false }, //
-    Displayname: { type: String, required: true },
+    // Displayname: { type: String, required: true },
+    Displayname: { type: String },
     Propertytype: { type: String, required: true }, //
     Noofrooms: { type: Number, required: true },
     Noofrestaurants: { type: Number, required: false },
